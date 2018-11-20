be07109f3677d0dc1038538377bfdc6deedf4ff9
witspirit <witspirit@gmail.com>
2018-11-20 21:33:00 +0100

    Initial commit after loading skeleton Spring Boot project.

---
+++ devoxx-showcase/src/main/java/be/witspirit/devoxx/devoxxshowcase/DevoxxShowcaseApplication.java
@@ -0,0 +1,12 @@
+package be.witspirit.devoxx.devoxxshowcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevoxxShowcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevoxxShowcaseApplication.class, args);
+	}
+}
